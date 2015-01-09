1ecddd147df08d47815a186b3b31b85c84224e3c
Henrique Rocha <hmrocha@gmail.com>
2015-01-09 14:39:51 +0100

    Refactor from Thread to AsyncTask.

--- app/src/main/java/me/henriquerocha/androidpitnews/MainActivity.java
+++ app/src/main/java/me/henriquerocha/androidpitnews/MainActivity.java
@@ -17,16 +17,18 @@
 package me.henriquerocha.androidpitnews;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 
 public class MainActivity extends Activity {
+
+    private TextView rssFeedTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,32 +36,33 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        final TextView textView = (TextView) findViewById(R.id.rss_feed);
+        rssFeedTextView = (TextView) findViewById(R.id.rss_feed);
 
-        new Thread() {
-            @Override
-            public void run() {
-                try {
-                    Request request = new Request.Builder()
-                            .url("http://www.androidpit.com/feed/main.xml")
-                            .build();
-
-                    OkHttpClient client = new OkHttpClient();
-                    Response response = client.newCall(request).execute();
-                    final String rssFeed = response.body().string();
-
-                    runOnUiThread(
-                            new Runnable() {
-                                @Override
-                                public void run() {
-                                    textView.setText(rssFeed);
-                                }
-                            });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
+        new GetFeedTask().execute();
     }
 
+    private class GetFeedTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                Request request = new Request.Builder()
+                        .url("http://www.androidpit.com/feed/main.xml")
+                        .build();
+
+                return new OkHttpClient().newCall(request).execute().body().string();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null) {
+                rssFeedTextView.setText(s);
+            }
+        }
+    }
 }
